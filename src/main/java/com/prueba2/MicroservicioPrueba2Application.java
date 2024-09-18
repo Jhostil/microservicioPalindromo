0290eaec26d24589d81f9a345680e83ef59e3586
Jhoiner <jhooiineer@gmail.com>
2024-09-17 21:43:12 -0500

    Primer comit

---
+++ src/main/java/com/prueba2/MicroservicioPrueba2Application.java
@@ -0,0 +1,13 @@
+package com.prueba2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPrueba2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPrueba2Application.class, args);
+	}
+
+}
